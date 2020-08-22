cb64baffcf1b28beb6d5517ef7b5f7961e1bf5ff
camiaguado <camiaguado@hotmail.com>
2020-08-22 15:53:42 +0200

    Create first controller

---
+++ API/controllers/pacienteControllers.js
@@ -0,0 +1,9 @@
+
+
+// When create a new client:
+exports.nuevoCliente = (req, res, next) => {
+    // TODO: insert into the data base
+    
+    res.json({ message: 'The client has been added successfuly'});
+
+}
