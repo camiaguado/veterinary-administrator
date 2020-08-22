a2bd1c457b0c70a2d191423dc397784b784502c3
camiaguado <camiaguado@hotmail.com>
2020-08-22 20:14:25 +0200

    Create form for new appointments

--- frontend/src/components/Pacientes.js
+++ frontend/src/components/Pacientes.js
@@ -1,4 +1,5 @@
 import React, {Fragment} from 'react';
+import { Link } from 'react-router-dom';
 
 const Pacientes = (props) => {
     
@@ -11,13 +12,13 @@ const Pacientes = (props) => {
             <div className="container mt-5 py-5">
                 <div className="row">
                     <div className="col-12 mb-5 d-flex justify-content-center">
-                        <a href="#" className="btn btn-success text-uppercase py-2 px-5 font-weight-bold">Crear Cita</a>
+                        <Link to={'/nueva'} className="btn btn-success text-uppercase py-2 px-5 font-weight-bold">Crear Cita</Link>
                     </div>
 
                     <div className="col-md-8 mx-auto">
                         <div className="list-group">
                             {props.citas.map((cita) => (
-                                <a key={cita._id} className="p-5 list-group-item list-group-item-action flex-column align-items-start">
+                                <Link to={''} key={cita._id} className="p-5 list-group-item list-group-item-action flex-column align-items-start">
                                     <div className="d-flex w-100 justify-content-between mb-4">
                                          <h3 className="mb-3">{cita.nombre}</h3>
                                          <small className="fecha-alta">
@@ -32,7 +33,7 @@ const Pacientes = (props) => {
                                         <p>Dueño: {cita.propietario}</p>
                                         <p>Telefono: {cita.telefono}</p>
                                     </div>
-                                </a>
+                                </Link>
                             ))}
                         </div>
                     </div>
