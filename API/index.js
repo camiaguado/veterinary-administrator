682452f1f79a9f2bc6e25096182739891e7cd2e6
camiaguado <camiaguado@hotmail.com>
2020-08-22 15:42:39 +0200

    Config connection to Mongodb

--- API/index.js
+++ API/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 // Create Server:
 const app = express();
 
+// Connect to MongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/veterinary', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
 // Port and start server:
 app.listen(4000, () => {
     console.log('The server is working...');
