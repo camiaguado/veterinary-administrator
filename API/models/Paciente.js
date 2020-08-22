7771910fe89f2ec2086927ba0474a23bc39a7840
camiaguado <camiaguado@hotmail.com>
2020-08-22 15:49:40 +0200

    Define first model

---
+++ API/models/Paciente.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const pacientesSchema = new Schema({
+    nombre: {
+        type: String,
+        trim: true // it is to delete spaces between words, ex: "Juan     Perez" ---> "Juan Perez" (Done by Mongo)
+    },
+    propietario: {
+        type: String,
+        trim: true
+    },
+    fecha: {
+        type: String,
+        trim: true
+    },
+    hora: {
+        type: String,
+        trim: true
+    },
+    sintomas: {
+        type: String,
+        trim: true
+    }
+});
+
+module.exports = mongoose.model('Paciente', pacientesSchema);
